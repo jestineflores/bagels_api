58c00deeef4cc00d70b930ae793d038fe3c61738
jsabath <justin.sabath@aol.com>
2020-05-19 10:59:40 -0600

    increment button

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const bagelInfo = {title:'This is the best bagel app', like_count:22, image: 'https://images.unsplash.com/photo-1518562923427-19e694fbd8e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'}
+
+const bagelImage = document.getElementById('bagel-image')
+bagelImage.src = bagelInfo.image
+
 const bagelTitle = document.getElementById('title')
 bagelTitle.innerText = bagelInfo.title
 
-const bagelImage = document.getElementById('image')
-bagelImage.src = bagelInfo.image
-const likeCount = document.getElementById('like-count')
-likeCount.innerText = bagelInfo.like_count
+const bagelLikes = document.getElementById('like-count')
+bagelLikes.innerHTML = bagelInfo.like_count
+
+const likeButton = document.getElementById('like-button')
+likeButton.addEventListener("click", (event)=> bagelLikes.innerHTML++)
+
+function incrementLikes(){
+ 
+//     event.preventDefault
+//     bagelLikes.textContent = parseint(bagelLikes.textContent +1)
+}
+// const bagelTitle = document.getElementById('title')
+// bagelTitle.innerText = bagelInfo.title
+
+// const bagelImage = document.getElementById('image')
+// bagelImage.src = bagelInfo.image
+// const likeCount = document.getElementById('like-count')
+// likeCount.innerText = bagelInfo.like_count
 const bagelURL = 'http://bagel-api-fis.herokuapp.com/bagels'
 
 fetch(bagelURL)
